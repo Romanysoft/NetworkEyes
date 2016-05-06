39bc9796b0686dd65c654abd19e90a0ecd106855
iansun <lauer3912@gmail.com>
2016-05-06 19:34:25 +0800

    Update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"NetworkEyes",
+        appID:"com.romanysoft.app.macos.NetworkEyes",
+        documentTitle:"NetworkEyes By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-76676167-11",
+        gitHome:"//github.com/Romanysoft/NetworkEyes",
+        reportIssueUrl:"//github.com/Romanysoft/NetworkEyes/issues",
+        changeLogUrl:"//github.com/Romanysoft/NetworkEyes/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/NetworkEyes/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
